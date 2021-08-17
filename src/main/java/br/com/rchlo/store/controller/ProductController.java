87bef34fb72f8cc5ac3bad0b996298b9762075ee
Simplício Baiano <simpliciobaiano42@gmail.com>
2021-08-17 17:51:42 -0300

    Cache da lista de produtos

--- src/main/java/br/com/rchlo/store/controller/ProductController.java
+++ src/main/java/br/com/rchlo/store/controller/ProductController.java
@@ -3,6 +3,8 @@ package br.com.rchlo.store.controller;
 import br.com.rchlo.store.dto.ProductByColorDto;
 import br.com.rchlo.store.dto.ProductDto;
 import br.com.rchlo.store.repository.ProductRepository;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,13 +21,21 @@ public class ProductController {
     }
 
     @GetMapping("/products")
+    @Cacheable(value = "list_all_products")
     public List<ProductDto> products() {
+        System.out.println("fez a consulta");
         return productRepository.findAllByOrderByName().stream().map(ProductDto::new).collect(Collectors.toList());
     }
 
     @GetMapping("/reports/products/by-color")
     public List<ProductByColorDto> productByColorReport() {
         return productRepository.productsByColor();
+    }
+
+    @GetMapping("/products/clear-cache")
+    @CacheEvict(value = "list_all_products", allEntries = true)
+    public String clearCache() {
+        return "<h2>Cache Limpa</h2>";
     }
 
 }
